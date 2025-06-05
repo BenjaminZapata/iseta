8351bbc553a8f91cd2b88fec978f81e39b911512
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-06-05 20:28:04 -0300

    Filtros estilado

--- resources/views/Componentes/form/filters.blade.php
+++ resources/views/Componentes/form/filters.blade.php
@@ -1,6 +1,6 @@
 <div class="w-100p flex-row p-2 gap-2 just-end">
         
-        <div class="flex-col items-end"><button id="show-filters" class="p-2 rounded">Filtros</button>
+        <div class="flex-col items-end mb-2"><button id="show-filters" class="rounded btn_blue mb-4">Filtros</button>
         <form action="{{route($url)}}" id="filters" class="none w-100p rounded bg-white">
         @if ($dropdowns)
             <div class="grid-4 gap-3 w-100p p-2">
@@ -13,15 +13,15 @@
                 <?= $form->select('filter_field','Criterio:','label-input-y-100',$filters,$fields,$options=[]) ?>
                 <?= $form->text('filter_search_box', 'Busqueda:','label-input-y-100',$filters) ?>
                 <div class="flex items-end just-center">
-                    <button class="p-2 rounded">Aplicar</button>
+                    <button class="p-2 rounded btn_blue">Aplicar</button>
                 </div>
                 
             </div>
     </form></div>
 
     
-    <div class="flex items-end just-center">
-        <a href="{{route($url)}}"><button class="p-2 rounded">Eliminar filtros</button></a>
+    <div class="flex items-end just-center self-start">
+        <a href="{{route($url)}}"><button class="rounded btn_blue filterButtonErase">Eliminar filtros</button></a>
     </div>
     
 </div>
