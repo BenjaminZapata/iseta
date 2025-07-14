f83d1e2fcf416e01abb1e6378d2ce829b6fe6381
Manuel Alvarez <Manu_07_alvarez@hotmail.com>
2025-07-14 19:56:03 -0300

    Refactor AlumnoCrudController for improved code readability; adjust migration to remove nullable constraint on nombre_institucion_secundario

--- app/Http/Controllers/Admin/AlumnoCrudController.php
+++ app/Http/Controllers/Admin/AlumnoCrudController.php
@@ -23,9 +23,10 @@ class AlumnoCrudController extends BaseController
         'filter_estado_civil' => 0
     ];
 
-    public $mensajes = ['mensaje'=>[],'error'=>[],'aviso'=>[]];
+    public $mensajes = ['mensaje' => [], 'error' => [], 'aviso' => []];
 
-    public function __construct(AlumnoRepository $alumnosRepo) {
+    public function __construct(AlumnoRepository $alumnosRepo)
+    {
         parent::__construct();
         $this->alumnosRepo = $alumnosRepo;
     }
@@ -58,11 +59,11 @@ class AlumnoCrudController extends BaseController
         $data = $request->validated();
         $response = redirect()->back();
 
-        if(Alumno::where('dni', strtolower($data['dni']))->first()){
-            return $response -> with('aviso','Ya hay un usuario con ese numero de documento')->withInput();
+        if (Alumno::where('dni', strtolower($data['dni']))->first()) {
+            return $response->with('aviso', 'Ya hay un usuario con ese numero de documento')->withInput();
         } else {
             Alumno::create($data);
-            return $response->with('mensaje','Se creo el alumno');
+            return $response->with('mensaje', 'Se creo el alumno');
         }
     }
 
@@ -72,33 +73,33 @@ class AlumnoCrudController extends BaseController
     public function edit(Request $request, Alumno $alumno)
     {
         $cursadas = Cursada::select(
-                'asignaturas.nombre as asignatura',
-                'cursadas.aprobada' ,
-                'cursadas.condicion' ,
-                'cursadas.anio_cursada' ,
-                'cursadas.id' ,
-                'carreras.nombre as carrera',
-                'asignaturas.anio as anio_asig'
-            )
-            ->join('asignaturas', 'cursadas.id_asignatura','asignaturas.id')
-            ->join('carrera_asignatura_profesor as cap','asignaturas.id','cap.id_asignatura')
-            ->join('carreras','cap.id_carrera','carreras.id')
-            -> where('cursadas.id_alumno',$alumno->id)
-            -> orderBy('carreras.id')
-            -> orderBy('asignaturas.anio')
-            -> orderBy('asignaturas.id')
-            -> orderBy('cursadas.anio_cursada')
-            -> get();
+            'asignaturas.nombre as asignatura',
+            'cursadas.aprobada',
+            'cursadas.condicion',
+            'cursadas.anio_cursada',
+            'cursadas.id',
+            'carreras.nombre as carrera',
+            'asignaturas.anio as anio_asig'
+        )
+            ->join('asignaturas', 'cursadas.id_asignatura', 'asignaturas.id')
+            ->join('carrera_asignatura_profesor as cap', 'asignaturas.id', 'cap.id_asignatura')
+            ->join('carreras', 'cap.id_carrera', 'carreras.id')
+            ->where('cursadas.id_alumno', $alumno->id)
+            ->orderBy('carreras.id')
+            ->orderBy('asignaturas.anio')
+            ->orderBy('asignaturas.id')
+            ->orderBy('cursadas.anio_cursada')
+            ->get();
 
-            $examenes = Examen::select('examenes.fecha','asignaturas.nombre as asignatura', 'examenes.nota' ,'examenes.id' ,'carreras.nombre as carrera','asignaturas.anio as anio_asig')
-            ->join('asignaturas', 'examenes.id_asignatura','asignaturas.id')
-            -> join('carrera_asignatura_profesor as cap','asignaturas.id','cap.id_asignatura')
-            -> join('carreras', 'cap.id_carrera','carreras.id')
-            -> where('examenes.id_alumno',$alumno->id)
-            -> orderBy('carreras.id')
-            -> orderBy('asignaturas.anio')
-            -> orderBy('examenes.fecha','desc')
-            -> get();
+        $examenes = Examen::select('examenes.fecha', 'asignaturas.nombre as asignatura', 'examenes.nota', 'examenes.id', 'carreras.nombre as carrera', 'asignaturas.anio as anio_asig')
+            ->join('asignaturas', 'examenes.id_asignatura', 'asignaturas.id')
+            ->join('carrera_asignatura_profesor as cap', 'asignaturas.id', 'cap.id_asignatura')
+            ->join('carreras', 'cap.id_carrera', 'carreras.id')
+            ->where('examenes.id_alumno', $alumno->id)
+            ->orderBy('carreras.id')
+            ->orderBy('asignaturas.anio')
+            ->orderBy('examenes.fecha', 'desc')
+            ->get();
 
         return view('Admin.Alumnos.edit', [
             'alumno' => $alumno,
@@ -118,14 +119,10 @@ class AlumnoCrudController extends BaseController
     {
         $data = $request->validated();
 
-        $mensajes = ['aviso'=>[],'error'=>[],'mensaje'=>[]];
+        $mensajes = ['aviso' => [], 'error' => [], 'mensaje' => []];
 
-        if($data['telefono1'] && Alumno::where('id','!=',$alumno->id)->where('telefono1', strtolower($data['telefono1']))->exists()){
-            $mensajes['aviso'][] = 'Ya hay un usuario con ese numero de telefono';
-        };
-
-        if($data['email'] && Alumno::where('email',strtolower($data['email'])) -> where('id','<>',$alumno->id)->exists() ){
-            return \redirect()->back()->with('error','El mail ya esta en usa')->withInput();
+        if ($data['dni'] && Alumno::where('id', '!=', $alumno->id)->where('dni', $data['dni'])->exists()) {
+            $mensajes['aviso'][] = 'Ya hay un usuario con ese numero de dni';
         }
 
         $alumno->update($data);
@@ -140,25 +137,24 @@ class AlumnoCrudController extends BaseController
     {
 
         $alumno->delete();
-        return redirect() -> route('admin.alumnos.index') -> with('mensaje', 'Se ha eliminado el alumno');
+        return redirect()->route('admin.alumnos.index')->with('mensaje', 'Se ha eliminado el alumno');
     }
 
 
-    public function verificar(Request $request, Alumno $alumno){
+    public function verificar(Request $request, Alumno $alumno)
+    {
 
-        if( 1 != $alumno->verificado){
+        if (1 != $alumno->verificado) {
             $alumno->verificar();
             $this->mensajes['mensaje'][] = 'Se ha verificado al alumno';
         }
 
-        if($alumno->password == 0){
+        if ($alumno->password == 0) {
             $alumno->password = bcrypt($alumno->dni);
             $alumno->save();
             $this->mensajes['mensaje'][] = 'Se utilizará su dni como clave de acceso';
         }
         // dd($this->mensajes,['mensaje'=>['Se ha verificado al alumno','Se utilizará su dni como clave de acceso']]);
         return redirect()->route('admin.alumnos.index')->with('mensajes', $this->mensajes);
-
     }
-
 }
