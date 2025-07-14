e1ab09dd72776b1c1340a74f187f7cb6defd85ce
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-07-14 00:35:01 -0300

    correccion barra lateral

--- resources/views/Componentes/aside.blade.php
+++ resources/views/Componentes/aside.blade.php
@@ -1,30 +1,30 @@
 <aside class="none lg-block admin-aside">
     <h1 class="logo-iseta">ISETA Admin</h1>
     <ul>
-        <li><a class="text-blue-600" href="{{route('admin.alumnos.index')}} "><i class="ti ti-user"></i>Alumnos</a></li>
-        <li><a class="text-blue-600" href="{{route('admin.profesores.index')}}"><i class="ti ti-users"></i>    Profesores  </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.carreras.index')}}"><i class="ti ti-address-book"></i>      Carreras    </a></li>
-        {{-- <li><a class="text-blue-600" href="{{route('admin.asignaturas.index')}}">   Asignaturas </a></li> --}}
-        <li><a class="text-blue-600" href="{{route('admin.mesas.index')}}"><i class="ti ti-clipboard-text"></i>         Mesas       </a></li>
-        {{-- <li><a class="text-blue-600" href="{{route('admin.examenes.index')}}"><i class="ti ti-address-book"></i>      Examenes    </a></li> --}}
-        <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}"><i class="ti ti-books"></i></i>      Cursadas    </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.inscriptos.index')}}"><i class="ti ti-file-invoice"></i>      Inscriptos    </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.alumnos.index')}} " style="display: block;"><i class="ti ti-user"></i>Alumnos</a></li>
+        <li><a class="text-blue-600" href="{{route('admin.profesores.index')}}" style="display: block;"><i class="ti ti-users"></i> Profesores </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.carreras.index')}}" style="display: block;"><i class="ti ti-address-book"></i> Carreras </a></li>
+        {{-- <li><a class="text-blue-600" href="{{route('admin.asignaturas.index')}}" style="display: block;"> Asignaturas </a></li> --}}
+        <li><a class="text-blue-600" href="{{route('admin.mesas.index')}}" style="display: block;"><i class="ti ti-clipboard-text"></i> Mesas </a></li>
+        {{-- <li><a class="text-blue-600" href="{{route('admin.examenes.index')}}" style="display: block;"><i class="ti ti-address-book"></i> Examenes </a></li> --}}
+        <li><a class="text-blue-600" href="{{route('admin.cursadas.index')}}" style="display: block;"><i class="ti ti-books"></i></i> Cursadas </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.inscriptos.index')}}" style="display: block;"><i class="ti ti-file-invoice"></i> Inscriptos </a></li>
         <hr>
-        <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  "><i class="ti ti-user-cog"></i>      Admins      </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.config.index')}}  "><i class="ti ti-settings"></i>      Configuracion      </a></li>
-        <li><a class="text-blue-600" href="{{route('admin.habiles.index')}}  "><i class="ti ti-calendar-time"></i>      Dias no habiles      </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.admins.index')}}  " style="display: block;"><i class="ti ti-user-cog"></i> Admins </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.config.index')}}  " style="display: block;"><i class="ti ti-settings"></i> Configuracion </a></li>
+        <li><a class="text-blue-600" href="{{route('admin.habiles.index')}}  " style="display: block;"><i class="ti ti-calendar-time"></i> Dias no habiles </a></li>
 
         <div class="aside-end">
             <li>
-                <a class="text-blue-600" href="{{route('admin.config.modoseguro')}}"><i class="ti ti-shield-lock"></i>
+                <a class="text-blue-600" href="{{route('admin.config.modoseguro')}}" style="display: block;"><i class="ti ti-shield-lock"></i>
                     @if ($config['modo_seguro'])
-                        Desactivar modo seguro
+                    Desactivar modo seguro
                     @else
-                          Activar modo seguro
+                    Activar modo seguro
                     @endif
                 </a>
             </li>
-            <li><a href="/admin/logout"><i class="ti ti-logout"></i> Cerrar sesion</a></li>
+            <li><a href="/admin/logout" style="display: block;"><i class="ti ti-logout"></i> Cerrar sesion</a></li>
         </div>
     </ul>
 
