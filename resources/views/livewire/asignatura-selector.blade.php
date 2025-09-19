1a06def7d93d388d4babda79414b50e3aa12c736
Manuel Alvarez <Manu_07_alvarez@hotmail.com>
2025-09-19 20:58:39 -0300

    refactor: enhance asignatura selection form by improving year input handling and removing redundant fields

--- resources/views/livewire/asignatura-selector.blade.php
+++ resources/views/livewire/asignatura-selector.blade.php
@@ -2,13 +2,37 @@
     @csrf
     <div class="perfil__info">
         <div class="perfil_dataname">
+            <label>Carrera:</label>
+            <p class="campo_info-noinput rounded"> {{ $carrera->nombre }} </p>
+            <input type="hidden" name="carrera_id" value="{{ $carrera->id }}">
+        </div>
+                <div class="perfil_dataname">
+            <label>Año:</label>
+            <select type="number" name="anio" wire:model.change="$anio" class="campo_info rounded">
+                <option value="{{ $anio }}">
+                    @if ($anio  == null )
+                        ingrese el año de asignatura a buscar
+                    @else
+                        {{ $anio }}º año
+                    @endif
+                </option>
+                <option value="1">1º año</option>
+                <option value="2">2º año</option>
+                <option value="3">3º año</option>
+                <option value="4">4º año</option>
+                <option value="5">5º año</option>
+            </select>
+        </div>
+        <div class="perfil_dataname">
             <label for="selectedId">Asignatura:</label>
             <select name="asignatura_id" id="selectedId" class="campo_info rounded" wire:model.change="selectedId"
                 form="add_asignatura">
                 @foreach ($asignaturas as $selectedId)
-                    <option value="{{ $selectedId->id }}">
-                        {{ $selectedId->nombre }}
-                    </option>
+                    @if ($selectedId->anio == $anio)
+                        <option value="{{ $selectedId->id }}">
+                            {{ $selectedId->nombre }}
+                        </option>
+                    @endif
                 @endforeach
             </select>
         </div>
@@ -25,17 +49,6 @@
             <label>Carga horaria:</label>
             <input name="carga_horaria" class="campo_info rounded" value="{{ $carga_horaria }}" type="number"
                 wire:model="$carga_horaria">
-        </div>
-
-        <div class="perfil_dataname">
-            <label>Año:</label>
-            <p class="campo_info-noinput rounded">{{ $anio }}</p>
-            <input type="hidden" name="anio" value="{{ $anio }}">
-        </div>
-        <div class="perfil_dataname">
-            <label>Carrera:</label>
-            <p class="campo_info-noinput rounded"> {{ $carrera->nombre }} </p>
-            <input type="hidden" name="carrera_id" value="{{ $carrera->id }}">
         </div>
     </div>
     <div class="botones-derecha"
