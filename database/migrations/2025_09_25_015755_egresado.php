90bb5fc9b31cbcabccc169cafd622685a178a150
Manuel Alvarez <manu07284alvarez@gmail.com>
2025-09-25 01:59:35 -0300

    feat: add migration for egresado table with estado and id_carrera fields

--- database/migrations/2025_09_25_015755_egresado.php
+++ database/migrations/2025_09_25_015755_egresado.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class estados extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('egresadoinscripto', function (Blueprint $table) {
             $table->integer('estado')->default(0);
@@ -17,18 +17,18 @@ class estados extends Migration
 
         // Ahora hacé el update
         DB::table('egresadoinscripto')->whereNotNull('anio_finalizacion')->update(['estado' => 1]);
-    }
 
+        Schema::table('egresadoinscripto', function (Blueprint $table) {
+            $table->integer('id_carrera');
+            $table->foreign('id_carrera')->references('id')->on('carreras');
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::table('egresadoinscripto', function (Blueprint $table) {
-            if (Schema::hasColumn('egresadoinscripto', 'estado')) {
-                $table->dropColumn('estado');
-            }
-        });
+        //
     }
 };
