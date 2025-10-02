c9a4886009498e3eb254a4da7021ac560cb98a79
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-10-02 19:36:15 -0300

    arreglo3

--- resources/views/Admin/Carreras/create.blade.php
+++ resources/views/Admin/Carreras/create.blade.php
@@ -6,14 +6,16 @@
         @include('components.header-avatar', ['tituloSeccion' => 'CREAR NUEVA CARRERA'])
         <div class="perfil__info">
 
-            <form action="{{ route('admin.carreras.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-
-                <?= $form->generate(null, 'post ', [
-                    'Información' => [$form->text('nombre', 'Nombre:', 'label-input-y-75'), $form->text('resolucion', 'Resolución:', 'label-input-y-75'), $form->text('anio_apertura', 'Año de apertura:', 'label-input-y-75'), $form->text('anio_fin', 'Año de cierre:', 'label-input-y-75'), $form->textarea('observaciones', 'Observaciones:', 'label-input-y-75'), $form->file('resolucion_archivo', 'Archivo de la resolución (PDF):', 'label-input-y-75')],
-                ]) ?>
-
-            </form>
+            <?= $form->generate(route('admin.carreras.store'), 'post', [
+                'Información' => [
+                    $form->text('nombre', 'Nombre:*', 'label-input-y-75', 'placeholder="Nombre de la carrera"'),
+                    $form->text('resolucion', 'Resolucion:*', 'label-input-y-75'),
+                    $form->text('anio_apertura', 'Año de apertura:*', 'label-input-y-75'),
+                    $form->text('anio_fin', 'Año de cierre:', 'label-input-y-75'),
+                    $form->textarea('observaciones', 'Observaciones:', 'label-input-y-75'),
+                    $form->file('resolucion_archivo', 'Archivo de la resolución (PDF):', 'label-input-y-75')
+                ]
+            ]) ?>
         </div>
     </div>
 </div>
