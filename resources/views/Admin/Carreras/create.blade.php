aa0bf8768584bf32270080ed2ad2d7ce8b0ed471
Nazareno <nazarenoleiva12@gmail.com>
2025-10-02 19:40:49 -0300

    fix: agregar asteriscos a campos obligatorios en el formulario de creación de carrera

--- resources/views/Admin/Carreras/create.blade.php
+++ resources/views/Admin/Carreras/create.blade.php
@@ -10,7 +10,7 @@
                     @csrf
 
                     <?= $form->generate(null, 'post ', [
-                            'Información' => [$form->text('nombre', 'Nombre:', 'label-input-y-75'), $form->text('resolucion', 'Resolución:', 'label-input-y-75'), $form->text('anio_apertura', 'Año de apertura:', 'label-input-y-75'), $form->text('anio_fin', 'Año de cierre:', 'label-input-y-75'), $form->textarea('observaciones', 'Observaciones:', 'label-input-y-75'), $form->file('resolucion_archivo', 'Archivo de la resolución (PDF):', 'label-input-y-75')],
+                            'Información' => [$form->text('nombre', 'Nombre*:', 'label-input-y-75'), $form->text('resolucion', 'Resolución*:', 'label-input-y-75'), $form->text('anio_apertura', 'Año de apertura*:', 'label-input-y-75'), $form->text('anio_fin', 'Año de cierre:', 'label-input-y-75'), $form->textarea('observaciones', 'Observaciones:', 'label-input-y-75'), $form->file('resolucion_archivo', 'Archivo de la resolución (PDF):', 'label-input-y-75')],
                         ]) ?>
                 </form>
             </div>
