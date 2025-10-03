ed5ca62b1afa6cbb964157f36280fee078b1815c
Manuel Alvarez <manu07284alvarez@gmail.com>
2025-10-03 13:41:13 -0300

    changes to migrations and add octane

--- database/migrations/2024_10_02_193754_correlativas.php
+++ database/migrations/2024_10_02_193754_correlativas.php
@@ -12,31 +12,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('correlatividad', function (Blueprint $table) {
-            $table->renameColumn('asignatura_correlativa', 'id_asignatura_correlativa');
-            $table->dropColumn('id');
-            $table->addColumn('id_carrera', 'integer')->nullable();
-            $table->addColumn('id_carrera_correlativa', 'integer')->nullable();
-            $table->
-            $table->primary(['id_asignatura', 'id_asignatura_correlativa', 'id_carrera', 'id_carrera_correlativa']);
+            $table->addColumn('integer', 'id_asignatura_correlativa');
+            $table->addColumn('integer', 'id_asignatura');
+            $table->addColumn('integer', 'id_carrera');
+            $table->primary(['id_asignatura', 'id_asignatura_correlativa', 'id_carrera']);
 
             // Foreign keys
             $table->foreign('id_asignatura')->references('id')->on('asignaturas');
             $table->foreign('id_asignatura_correlativa')->references('id')->on('asignaturas');
             $table->foreign('id_carrera')->references('id')->on('carreras');
-            $table->foreign('id_carrera_correlativa')->references('id')->on('carreras');
 
         });
 
         DB::statement('
-            INSERT INTO correlatividad (id_asignatura, id_asignatura_correlativa, id_carrera, id_carrera_correlativa)
+            INSERT INTO correlatividad (id_asignatura, id_asignatura_correlativa, id_carrera)
             SELECT
                 c.id_asignatura,
                 c.asignatura_correlativa,
-                a1.id_carrera,
-                a2.id_carrera
+                a1.id_carrera
             FROM correlatividades c
-            JOIN asignaturas a1 ON c.id_asignatura = a1.id
-            JOIN asignaturas a2 ON c.asignatura_correlativa = a2.id
+            JOIN asignaturas a1 ON c.id_asignatura = a1.id;
         ');
 
         Schema::dropIfExists('correlatividades');
