c1deff3554c0cb4cb5451a01663961b57f313e3e
javiertorres123 <javiiieaea123@gmail.com>
2025-10-08 18:50:35 -0300

    change

--- resources/views/livewire/asignatura-selector.blade.php
+++ resources/views/livewire/asignatura-selector.blade.php
@@ -8,20 +8,20 @@
                 <input type="hidden" name="id_carrera" value="{{ $carrera->id }}">
             </div>
             <div class="perfil_dataname">
-                <label>Año:</label>
-             <select name="anio" class="campo_info rounded" wire:model="anio" @if($selectedId) disabled @endif>
-    @if (is_null($anio))
-        <option value="">Elija el año de la asignatura</option>
-    @endif
+    <label>Año:</label>
+    <select name="anio" class="campo_info rounded" wire:model="anio" @if($selectedId) disabled @endif>
+        @if (is_null($anio))
+            <option value="">Elija el año de la asignatura</option>
+        @endif
 
-    @for ($i = 1; $i <= 5; $i++)
-        <option value="{{ $i - 1 }}" {{ $anio == ($i - 1) ? 'selected' : '' }}>
-            {{ $i }}º año
-        </option>
-    @endfor
-</select>
+        @for ($i = 1; $i <= 5; $i++)
+            <option value="{{ $i }}" {{ $anio == $i ? 'selected' : '' }}>
+                {{ $i }}º año
+            </option>
+        @endfor
+    </select>
+</div>
 
-            </div>
 
             <div class="perfil_dataname">
                 <label for="selectedId">Asignatura:</label>
