5ac1ace7789fa8645f349ac17435d23396f305ba
javiertorres123 <javiiieaea123@gmail.com>
2025-10-17 12:09:40 -0300

    change

--- resources/views/Admin/template.blade.php
+++ resources/views/Admin/template.blade.php
@@ -54,7 +54,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
-    <script src="{{ asset('js/ModificarUsuarios.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('js/usuarios/ModificarUsuarios.js') }}?v={{ time() }}"></script>
 
 
 
