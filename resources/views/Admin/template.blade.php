ed58d09392eb3b12c8e484f86b985d97c153ff4e
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-10-21 19:47:25 -0300

    mejora

--- resources/views/Admin/template.blade.php
+++ resources/views/Admin/template.blade.php
@@ -36,14 +36,21 @@
 
 
 
-    <script src="{{asset('js/ocultar-mensaje.js')}}"></script>
-    <script src="{{asset('js/confirmacion.js')}}"></script>
-    <script src="{{asset('js/filters.js')}}"></script>
+    <script src="{{ asset('js/ocultar-mensaje.js') }}"></script>
+    <script src="{{ asset('js/confirmacion.js') }}"></script>
+    <script src="{{ asset('js/filters.js') }}"></script>
+    <script src="{{ asset('js/Admin/btn-contraible.js') }}"></script>
     <script defer src="{{ asset('js/header-avatar.js') }}?v={{ time() }}"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
+
+
+
 </body>
 
 </html>
