d7dd5fc28357a91ab0e2026afa80c43cf399b35d
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-10-22 17:42:02 -0300

    mejora de botones

--- resources/views/Admin/Alumnos/index.blade.php
+++ resources/views/Admin/Alumnos/index.blade.php
@@ -1,139 +1,143 @@
 @extends('Admin.template')
 
 @section('content')
-    <style>
-        #filters .label-input-y-100 label {
-            text-align: left !important;
-            display: block !important;
-            width: 100%;
-            padding-top: 15px;
-        }
-    </style>
+<style>
+    #filters .label-input-y-100 label {
+        text-align: left !important;
+        display: block !important;
+        width: 100%;
+        padding-top: 15px;
+    }
+</style>
 
-    {{-- CONTENT --}}
+{{-- CONTENT --}}
 
-    <div class="table" data-name="tablaAlumnos">
+<div class="table" data-name="tablaAlumnos">
 
-        @include('components.header-avatar', ['tituloSeccion' => 'GESTIÓN DE ALUMNOS'])
+    @include('components.header-avatar', ['tituloSeccion' => 'GESTIÓN DE ALUMNOS'])
 
-        {{-- BOTON CREAR --}}
-        <div class="perfil__header-alt" style="display: flex; align-items: center; gap: 1rem;">
-            <a href="{{ route('admin.alumnos.create') }}">
-                <button class="btn_blue">
-                    <i class="ti ti-circle-plus" style="font-size: 1.3em; margin-right: 8px;"></i>Agregar alumno
-                </button>
-            </a>
+    {{-- BOTON CREAR --}}
+    <div class="perfil__header-alt" style="display: flex; align-items: center; gap: 1rem;">
+        <a href="{{ route('admin.alumnos.create') }}">
+            <button class="btn_blue">
+                <i class="ti ti-circle-plus" style="font-size: 1.3em; margin-right: 8px;"></i>Agregar alumno
+            </button>
+        </a>
 
-            {{-- FILTROS --}}
-            <?= $filtergen->generate('admin.alumnos.index', $filters, [
-                    'dropdowns' => [
-                        $form->select('filter_titulo', 'Estado del título:', 'label-input-y-100', old('filter_titulo', $filters->filter_titulo ?? null), [
-                            null => 'Todos',
-                            0 => 'No entregado',
-                            1 => 'Certificado de constancia de título en trámite',
-                            2 => 'Constancia de alumno del último año del nivel secundario',
-                            3 => 'Fotocopia del título original secundario',
-                        ]),
+        {{-- FILTROS --}}
+        <?= $filtergen->generate('admin.alumnos.index', $filters, [
+            'dropdowns' => [
+                $form->select('filter_titulo', 'Estado del título:', 'label-input-y-100', old('filter_titulo', $filters->filter_titulo ?? null), [
+                    null => 'Todos',
+                    0 => 'No entregado',
+                    1 => 'Certificado de constancia de título en trámite',
+                    2 => 'Constancia de alumno del último año del nivel secundario',
+                    3 => 'Fotocopia del título original secundario',
+                ]),
 
-                        $form->select('filter_vencido', 'Plazo de entrega del título:', 'label-input-y-100', old('filter_vencido', $filters->filter_vencido ?? 'null'), [
-                            null => 'Todos',
-                            1 => 'Vencido',
-                        ]),
-                    ],
-                    'fields' => [
-                        'nombre' => 'Nombre',
-                        'apellido' => 'Apellido',
-                        'dni' => 'Dni',
-                    ],
-                ]) ?>
-        </div>
+                $form->select('filter_vencido', 'Plazo de entrega del título:', 'label-input-y-100', old('filter_vencido', $filters->filter_vencido ?? 'null'), [
+                    null => 'Todos',
+                    1 => 'Vencido',
+                ]),
+            ],
+            'fields' => [
+                'nombre' => 'Nombre',
+                'apellido' => 'Apellido',
+                'dni' => 'Dni',
+            ],
+        ]) ?>
+    </div>
 
-        {{-- TABLA --}}
-        <table class="table__body">
+    {{-- TABLA --}}
+    <table class="table__body">
 
-            {{-- HEADER --}}
-            <thead>
-                <tr>
-                    <th>Alumno</th>
-                    <th>Contacto</th>
-                    <th>Dirección</th>
-                    <th>Academico</th>
-                    <th class="center">Acción</th>
-                </tr>
-            </thead>
+        {{-- HEADER --}}
+        <thead>
+            <tr>
+                <th>Alumno</th>
+                <th>Contacto</th>
+                <th>Dirección</th>
+                <th>Academico</th>
+                <th class="center">Acción</th>
+            </tr>
+        </thead>
 
-            {{-- TBODY --}}
-            <tbody>
-                @foreach ($alumnos as $alumno)
-                    <tr>
-                        <td class="capitalize">
-                            <p class="bold" style="text-transform: uppercase;">{{ $alumno->apellidoNombre() }}</p>
-                            <p>dni: {{ $alumno->dniPuntos() }}</p>
-                        </td>
+        {{-- TBODY --}}
+        <tbody>
+            @foreach ($alumnos as $alumno)
+            <tr>
+                <td class="capitalize">
+                    <p class="bold" style="text-transform: uppercase;">{{ $alumno->apellidoNombre() }}</p>
+                    <p>dni: {{ $alumno->dniPuntos() }}</p>
+                </td>
 
-                        <td>
-                            <p style="text-transform: none;">{{ $alumno->email ? $alumno->email : 'Sin mail registrado' }}
-                            </p>
-                            @if ($alumno->telefono1)
-                                <p>tel: {{ $alumno->telefono1 }}</p>
-                            @elseif ($alumno->telefono2)
-                                <p>tel: {{ $alumno->telefono2 }}</p>
-                            @elseif ($alumno->telefono3)
-                                <p>tel: {{ $alumno->telefono3 }}</p>
-                            @else
-                                <p>tel: Sin telefono</p>
-                            @endif
-                        </td>
-                        <td>
-                            <p>{{ $alumno->ciudad }}</p>
-                            <p>{{ $alumno->calle }} {{ $alumno->casa_numero ? $alumno->casa_numero : '' }}</p>
-                        </td>
-                        <td>
-                            @php
-                                $titulo = [
-                                    'No entregado',
-                                    'Fotocopia del título original secundario',
-                                    'Certificado de constancia de título en trámite',
-                                    'Constancia de alumno del último año del nivel secundario',
-                                ];
-                            @endphp
-                            <p class="bold" style="text-transform: uppercase;">titulo:
-                                {{ $titulo[$alumno->titulo_secundario] }}</p>
-                            <p>estado: {{ $alumno->egresado?->estado_texto ?? 'Sin inscripción' }}</p>
+                <td>
+                    <p style="text-transform: none;">{{ $alumno->email ? $alumno->email : 'Sin mail registrado' }}
+                    </p>
+                    @if ($alumno->telefono1)
+                    <p>tel: {{ $alumno->telefono1 }}</p>
+                    @elseif ($alumno->telefono2)
+                    <p>tel: {{ $alumno->telefono2 }}</p>
+                    @elseif ($alumno->telefono3)
+                    <p>tel: {{ $alumno->telefono3 }}</p>
+                    @else
+                    <p>tel: Sin telefono</p>
+                    @endif
+                </td>
+                <td>
+                    <p>{{ $alumno->ciudad }}</p>
+                    <p>{{ $alumno->calle }} {{ $alumno->casa_numero ? $alumno->casa_numero : '' }}</p>
+                </td>
+                <td>
+                    @php
+                    $titulo = [
+                    'No entregado',
+                    'Fotocopia del título original secundario',
+                    'Certificado de constancia de título en trámite',
+                    'Constancia de alumno del último año del nivel secundario',
+                    ];
+                    @endphp
+                    <p class="bold" style="text-transform: uppercase;">titulo:
+                        {{ $titulo[$alumno->titulo_secundario] }}
+                    </p>
+                    <p>estado: {{ $alumno->egresado?->estado_texto ?? 'Sin inscripción' }}</p>
 
 
 
-                        </td>
-                        <td class="flex just-center">
-                            <div style="display: flex; justify-content: center;">
-                                <a href="{{ route('admin.alumnos.edit', ['alumno' => $alumno->id]) }}">
-                                    <button class="btn_blue"><i class="ti ti-file-info"
-                                            style="font-size: 1.3em; margin-right: 8px;"></i>Modificar</button>
-                                </a>
-                                @if (!$config['modo_seguro'])
-                                    <form id="form-eliminar-{{ $alumno->id }}"
-                                        action="{{ route('admin.alumnos.destroy', $alumno->id) }}" method="POST"
-                                        style="display: inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button"
-                                            onclick="openGeneralModal('form-eliminar-{{ $alumno->id }}',
+                </td>
+                <td class="flex just-center" style="min-width: 120px;">
+                    <div style="display: flex; justify-content: center; gap: 10px;">
+                        <a href="{{ route('admin.alumnos.edit', ['alumno' => $alumno->id]) }}">
+                            <button class="btn_blue btn_contraible"><i class="ti ti-pencil"
+                                    style="font-size: 1.3em;"></i>
+                                <span class="btn-text">Editar</span>
+                            </button>
+                        </a>
+                        @if (!$config['modo_seguro'])
+                        <form id="form-eliminar-{{ $alumno->id }}"
+                            action="{{ route('admin.alumnos.destroy', $alumno->id) }}" method="POST"
+                            style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button"
+                                onclick="openGeneralModal('form-eliminar-{{ $alumno->id }}',
                                     '¿Estás seguro de que querés eliminar al alumno: {{ strtoupper($alumno->apellido) }} {{ strtoupper($alumno->nombre) }}? \n \n ESTA ACCIÓN NO SE PUEDE DESHACER.')"
-                                            class="btn_icon-danger" style="background-color: red; margin-left: 10px;">
-                                            <i class="ti ti-trash" style="font-size: 1.3em"></i>
-                                        </button>
-                                    </form>
-                                @endif
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                                class="btn_icon-danger btn_contraible" style="background-color: red;">
+                                <i class="ti ti-trash"></i>
+                                <span class="btn-text">Eliminar</span>
+                            </button>
+                        </form>
+                        @endif
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
-    {{-- PAGINACIÓN --}}
-    <div class="w-full flex justify-center p-5 pagination">
-        {{ $alumnos->appends(request()->query())->links('Componentes.pagination') }}
-    </div>
+{{-- PAGINACIÓN --}}
+<div class="w-full flex justify-center p-5 pagination">
+    {{ $alumnos->appends(request()->query())->links('Componentes.pagination') }}
+</div>
 @endsection
--- resources/views/Admin/Profesores/index.blade.php
+++ resources/views/Admin/Profesores/index.blade.php
@@ -1,95 +1,99 @@
 @extends('Admin.template')
 
 @section('content')
-    <div class="table" data-name="tablaProfesores">
+<div class="table" data-name="tablaProfesores">
 
-        @include('components.header-avatar', ['tituloSeccion' => 'GESTIÓN DE PROFESORES'])
+    @include('components.header-avatar', ['tituloSeccion' => 'GESTIÓN DE PROFESORES'])
 
-        <div class="perfil__header-alt">
-            <a href="{{ route('admin.profesores.create') }}">
-                <button class="btn_blue">
-                    <i class="ti ti-circle-plus" style="font-size: 1.3em; margin-right: 8px;"></i>Agregar profesor
-                </button>
-            </a>
-            {{-- FILTROS --}}
-            <?= $filtergen->generate('admin.profesores.index', $filters,[
-                    // 'dropdowns' => [
-                    //     $carreraM->dropdown('filter_carrera_id','Carrera:', 'label-input-y-100',$filters, ['first_items' => ['Todas']])
-                    // ],
-                    'fields' => [
-                        'profesor' => 'Profesor',
-                        'dni' => 'Dni',
-                    ],
-                ]) ?>
-        </div>
+    <div class="perfil__header-alt">
+        <a href="{{ route('admin.profesores.create') }}">
+            <button class="btn_blue">
+                <i class="ti ti-circle-plus" style="font-size: 1.3em; margin-right: 8px;"></i>Agregar profesor
+            </button>
+        </a>
+        {{-- FILTROS --}}
+        <?= $filtergen->generate('admin.profesores.index', $filters, [
+            // 'dropdowns' => [
+            //     $carreraM->dropdown('filter_carrera_id','Carrera:', 'label-input-y-100',$filters, ['first_items' => ['Todas']])
+            // ],
+            'fields' => [
+                'profesor' => 'Profesor',
+                'dni' => 'Dni',
+            ],
+        ]) ?>
+    </div>
 
-        <table class="table__body">
-            <thead>
-                <tr>
-                    <th>Profesor</th>
-                    <th>Contacto</th>
-                    <th>Dirección</th>
-                    <th class="center">Acción</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($profesores as $profesor)
-                    <tr>
-                        <td>
-                            <p class="bold" style="text-transform: uppercase;">{{ $profesor->apellidoNombre() }}</p>
-                            <p>dni: {{ $profesor->dniPuntos() }}</p>
-                        </td>
-                        <td>
-                            <p class="excluir-mayusculas">
-                                {{ $profesor->email ? $profesor->email : 'Sin mail registrado' }}
-                            </p>
-                            <p>
-                                tel: {{ $profesor->telefono1 ? $profesor->telefono1 : 'Sin teléfono' }}
-                            </p>
-                        </td>
-                        <td>
-                            <p>{{ $profesor->ciudad }}</p>
-                            <p>{{ $profesor->calle }} {{ $profesor->casa_numero ? $profesor->casa_numero : '' }}</p>
-                        </td>
-                        <td class="flex just-center">
-                            <div style="display: flex; justify-content: center;">
-                                <a href="{{ route('admin.profesores.edit', ['profesor' => $profesor->id]) }}">
-                                    <button class="btn_blue"><i class="ti ti-file-info"
-                                            style="font-size: 1.3em; margin-right: 8px;"></i>Modificar</button>
-                                </a>
-                                @if (!$config['modo_seguro'])
-                                    <form method="POST" id="form-eliminar-{{ $profesor->id }}"
-                                        action="{{ route('admin.profesores.destroy', ['profesor' => $profesor->id]) }}">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="button" class="btn_icon-danger"
-                                            onclick="openGeneralModal(
+    <table class="table__body">
+        <thead>
+            <tr>
+                <th>Profesor</th>
+                <th>Contacto</th>
+                <th>Dirección</th>
+                <th class="center">Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($profesores as $profesor)
+            <tr>
+                <td>
+                    <p class="bold" style="text-transform: uppercase;">{{ $profesor->apellidoNombre() }}</p>
+                    <p>dni: {{ $profesor->dniPuntos() }}</p>
+                </td>
+                <td>
+                    <p class="excluir-mayusculas">
+                        {{ $profesor->email ? $profesor->email : 'Sin mail registrado' }}
+                    </p>
+                    <p>
+                        tel: {{ $profesor->telefono1 ? $profesor->telefono1 : 'Sin teléfono' }}
+                    </p>
+                </td>
+                <td>
+                    <p>{{ $profesor->ciudad }}</p>
+                    <p>{{ $profesor->calle }} {{ $profesor->casa_numero ? $profesor->casa_numero : '' }}</p>
+                </td>
+                <td class="flex just-center" style="min-width: 120px;">
+                    <div style="display: flex; justify-content: center;">
+                        <a href="{{ route('admin.profesores.edit', ['profesor' => $profesor->id]) }}">
+                            <button class="btn_blue btn_contraible">
+                                <i class="ti ti-pencil" style="font-size: 1.3em;"></i>
+                                <span class="btn-text">Editar</span>
+                            </button>
+                        </a>
+                        @if (!$config['modo_seguro'])
+                        <form method="POST" id="form-eliminar-{{ $profesor->id }}"
+                            action="{{ route('admin.profesores.destroy', ['profesor' => $profesor->id]) }}">
+                            @csrf
+                            @method('delete')
+                            <button type="button" class="btn_icon-danger btn_contraible"
+                                onclick="openGeneralModal(
                 'form-eliminar-{{ $profesor->id }}',
                 '¿Estás seguro de que querés eliminar al profesor: {{ mb_strtoupper($profesor->apellido, 'UTF-8') }} {{ mb_strtoupper($profesor->nombre, 'UTF-8') }}? \n\nESTA ACCIÓN NO SE PUEDE DESHACER.'
             )"
-                                            style="background-color: red; margin-left: 10px;">
-                                            <i class="ti ti-trash" style="font-size: 1.3em;"></i>
-                                        </button>
-                                    </form>
-                                @endif
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                                style="background-color: red; margin-left: 10px;">
+                                <i class="ti ti-trash" style="font-size: 1.3em;"></i>
+                                <span class="btn-text">Eliminar</span>
+                            </button>
+                        </form>
+                        @endif
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
-    {{-- PAGINACIÓN --}}
-    <div class="w-full flex justify-center p-5 pagination">
-        {{ $profesores->appends(request()->query())->links('Componentes.pagination') }}
-    </div>
+{{-- PAGINACIÓN --}}
+<div class="w-full flex justify-center p-5 pagination">
+    {{ $profesores->appends(request()->query())->links('Componentes.pagination') }}
+</div>
 
-    {{-- SOLO PARA ESTA VISTA --}}
-    <style>
-        .perfil__header-alt select,
-        .perfil__header-alt input[type="text"] {
-            height: 40px; /* misma altura para criterio y búsqueda */
-        }
-    </style>
+{{-- SOLO PARA ESTA VISTA --}}
+<style>
+    .perfil__header-alt select,
+    .perfil__header-alt input[type="text"] {
+        height: 40px;
+        /* misma altura para criterio y búsqueda */
+    }
+</style>
 @endsection
