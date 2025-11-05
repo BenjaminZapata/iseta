d26c1cb1eecb4094f20c4c281da65f616e74de9d
Juan Martin Arrizubieta <juanarrizubieta@hotmail.com>
2025-11-05 17:49:30 -0300

    Merge branch 'Tester' of https://github.com/BenjaminZapata/iseta into CorreccionesJuan -COMBINANDO REGISTRAR CURSADAS

---
+++ app/Policies/CursadaPolicy.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Admin;
+use App\Models\Alumno;
+use App\Models\Cursada;
+use Illuminate\Auth\Access\Response;
+
+class CursadaPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(Alumno $alumno): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(Alumno $alumno, Cursada $cursada): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(Alumno $alumno): bool
+    {
+        return false;
+    }
+
+    public function createAdmin(Admin $admin): Response
+    {
+        return in_array($admin->rol, [0, 1]) ?
+            Response::allow() :
+            Response::deny('No autorizado.');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(Alumno $alumno, Cursada $cursada): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(Alumno $alumno, Cursada $cursada): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(Alumno $alumno, Cursada $cursada): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(Alumno $alumno, Cursada $cursada): bool
+    {
+        return false;
+    }
+}
